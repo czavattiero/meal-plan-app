c7c5ed93f422390c3276eb252a0d2d7b3590d738
Carolina Zavattiero <czavattiero@gmail.com>
2026-06-08 15:35:52 -0600

    Fix ArrayBuffer type for VAPID key

--- hooks/usePushSubscription.ts
+++ hooks/usePushSubscription.ts
@@ -10,7 +10,7 @@ function getOrCreateDeviceId(): string {
   return id
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
@@ -18,7 +18,7 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i)
   }
-  return outputArray
+  return outputArray.buffer
 }
 
 export function usePushSubscription() {
