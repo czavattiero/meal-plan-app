cc350567efd27a03db9ada48456fca616b48c47f
Carolina Zavattiero <czavattiero@gmail.com>
2026-06-08 12:45:18 -0600

    Fix VAPID env var handling

--- app/api/push/send/route.ts
+++ app/api/push/send/route.ts
@@ -2,11 +2,13 @@ import { kv } from '@vercel/kv'
 import webpush from 'web-push'
 import { NextResponse } from 'next/server'
 
-webpush.setVapidDetails(
-  process.env.VAPID_EMAIL!,
-  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
-  process.env.VAPID_PRIVATE_KEY!
-)
+if (process.env.VAPID_EMAIL && process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY && process.env.VAPID_PRIVATE_KEY) {
+  webpush.setVapidDetails(
+    process.env.VAPID_EMAIL,
+    process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+    process.env.VAPID_PRIVATE_KEY
+  )
+}
 
 export async function POST(request: Request) {
   try {
