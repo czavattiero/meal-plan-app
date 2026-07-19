11f7344761b4dee1d919089d79de46638ace5e6a
czavattiero <czavattiero@gmail.com>
2026-07-19 11:03:18 -0600

    Fix description formatting in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import NavBar from '@/components/NavBar'
 
 export const metadata: Metadata = {
   title: "Sophia's Meal Plan",
-  description: 'Sophia's meal plan',
+  description: "Sophia's meal plan",
   manifest: '/manifest.json',
   appleWebApp: {
     capable: true,
